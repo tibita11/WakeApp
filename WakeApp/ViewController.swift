e16a9ad3899392469897d743a9bee06f7fcb6209
tibita11 <tibita0811@gmail.com>
2023-05-27 09:19:50 +0900

    initial commit

---
+++ WakeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WakeApp
+//
+//  Created by 鈴木楓香 on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
