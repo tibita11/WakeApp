cc053546785f536ff20b52cd91f5e7d25a7cdfc9
tibita11 <tibita0811@gmail.com>
2023-05-31 23:54:17 +0900

    test: バリデーションチェックについてユニットテストを追加

--- WakeAppTests/WakeAppTests.swift
+++ WakeAppTests/WakeAppTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import WakeApp
 
-final class WakeAppTests: XCTestCase {
+final class AuthenticationTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,12 +18,57 @@ final class WakeAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testEmailValidator() {
+        XCTContext.runActivity(named: "空欄の場合") { _ in
+            let result = EmailValidator(value: "").validate()
+            XCTAssertFalse(result.isValid)
+        }
+        
+        XCTContext.runActivity(named: "空白の場合") { _ in
+            XCTContext.runActivity(named: "空白が全角の場合") { _ in
+                let result = EmailValidator(value: "　aaa").validate()
+                XCTAssertFalse(result.isValid)
+            }
+            XCTContext.runActivity(named: "空白が半角の場合") { _ in
+                let result = EmailValidator(value: " aaa").validate()
+                XCTAssertFalse(result.isValid)
+            }
+        }
+        
+        XCTContext.runActivity(named: "成功の場合") { _ in
+            let result = EmailValidator(value: "aaa").validate()
+            XCTAssertTrue(result.isValid)
+        }
+        
+    }
+    
+    func testPasswordValidator() {
+        XCTContext.runActivity(named: "空欄の場合") { _ in
+            let result = PasswordValidator(value: "").validate()
+            XCTAssertFalse(result.isValid)
+        }
+        
+        XCTContext.runActivity(named: "空白の場合") { _ in
+            XCTContext.runActivity(named: "空白が全角の場合") { _ in
+                let result = PasswordValidator(value: "　aaaaaaaaaaaa").validate()
+                XCTAssertFalse(result.isValid)
+            }
+            XCTContext.runActivity(named: "空白が半角の場合") { _ in
+                let result = PasswordValidator(value: " aaaaaaaaaaaa").validate()
+                XCTAssertFalse(result.isValid)
+            }
+        }
+        
+        XCTContext.runActivity(named: "12文字以下の場合") { _ in
+            let result = PasswordValidator(value: "aaaaaaaaaaa").validate()
+            XCTAssertFalse(result.isValid)
+        }
+        
+        XCTContext.runActivity(named: "成功の場合") { _ in
+            let result = PasswordValidator(value: "aaaaaaaaaaaa").validate()
+            XCTAssertTrue(result.isValid)
+        }
+        
     }
 
     func testPerformanceExample() throws {
