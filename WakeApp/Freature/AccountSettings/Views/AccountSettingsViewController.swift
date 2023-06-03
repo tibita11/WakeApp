2730e08b2c0ee8a7f6d303294023db8c43e0f919
tibita11 <tibita0811@gmail.com>
2023-06-03 12:01:39 +0900

    feat: アカウント設定画面作成

--- WakeApp/Freature/AccountSettings/Views/AccountSettingsViewController.swift
+++ WakeApp/Freature/AccountSettings/Views/AccountSettingsViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class AccountSettingsViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.layer.cornerRadius = Const.LargeBlueButtonCorner
+        }
+    }
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    
+    // MARK: - Action
+    
+    @IBAction func tapNextButton(_ sender: Any) {
+        
     }
     
 }
