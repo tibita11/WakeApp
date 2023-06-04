382baa60e12ff7a3461aed20660aafe9970f6163
tibita11 <tibita0811@gmail.com>
2023-06-04 19:24:39 +0900

    feat: birthdayTextFieldのtoolbarと値変更時の処理を実装

--- WakeApp/Freature/AccountSettings/ViewModels/AccountSettingsViewModel.swift
+++ WakeApp/Freature/AccountSettings/ViewModels/AccountSettingsViewModel.swift
@@ -5,16 +5,18 @@
 //  Created by 鈴木楓香 on 2023/06/03.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
 
 struct AccountSettingsViewModelInput {
     let userNameTextFieldObserver: Observable<String?>
+    let datePickerObserver: Observable<Date?>
 }
 
 protocol AccountSettingsViewModelOutput {
     var userNameValidationDriver: Driver<String> { get }
+    var birthdayTextFieldDriver: Driver<String> { get }
 }
 
 protocol AccountSettingsViewModelType {
@@ -26,10 +28,17 @@ class AccountSettingsViewModel: AccountSettingsViewModelType {
     var output: AccountSettingsViewModelOutput! { self }
     private let disposeBag = DisposeBag()
     private let userNameValidationRelay = PublishRelay<String>()
+    private let birthdayTextFieldRelay = PublishRelay<String>()
+    private lazy var dateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy年MM月dd日"
+        return dateFormatter
+    }()
     
     func setUp(input: AccountSettingsViewModelInput) {
         // UserNameバリデーションチェック
         input.userNameTextFieldObserver
+            .skip(2)
             .subscribe(onNext: { [weak self] userName in
                 guard let userName, let self else { return }
                 let result = UserNameValidator(value: userName).validate()
@@ -39,6 +48,18 @@ class AccountSettingsViewModel: AccountSettingsViewModelType {
                 case .invalid(let error):
                     userNameValidationRelay.accept("\(error.localizedDescription)")
                 }
+            })
+            .disposed(by: disposeBag)
+        // dateをString型で表示する
+        input.datePickerObserver
+            .subscribe(onNext: { [weak self] date in
+                guard let self else { return }
+                guard let date else {
+                    birthdayTextFieldRelay.accept("")
+                    return
+                }
+                let dateString = dateFormatter.string(from: date)
+                birthdayTextFieldRelay.accept(dateString)
             })
             .disposed(by: disposeBag)
     }
@@ -51,6 +72,10 @@ class AccountSettingsViewModel: AccountSettingsViewModelType {
 extension AccountSettingsViewModel: AccountSettingsViewModelOutput {
     var userNameValidationDriver: Driver<String> {
         userNameValidationRelay.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    var birthdayTextFieldDriver: Driver<String> {
+        birthdayTextFieldRelay.asDriver(onErrorDriveWith: .empty())
     }
     
 }
--- WakeApp/Freature/AccountSettings/Views/AccountSettingsViewController.swift
+++ WakeApp/Freature/AccountSettings/Views/AccountSettingsViewController.swift
@@ -20,6 +20,7 @@ class AccountSettingsViewController: UIViewController {
     @IBOutlet weak var userNameValidationLabel: UILabel!
     private let viewModel = AccountSettingsViewModel()
     private let disposeBag = DisposeBag()
+    private var datePicker = UIDatePicker()
     
     
     // MARK: - View Life Cycle
@@ -38,10 +39,11 @@ class AccountSettingsViewController: UIViewController {
     }
     
     private func setUp() {
+        setUpDatePicker()
         // ViewModel設定
-        let input = AccountSettingsViewModelInput(userNameTextFieldObserver: userNameTextField.rx.text.asObservable())
+        let input = AccountSettingsViewModelInput(userNameTextFieldObserver: userNameTextField.rx.text.asObservable(), datePickerObserver: datePicker.rx.controlEvent(.valueChanged).map {[weak self] in self?.datePicker.date })
         viewModel.setUp(input: input)
-        // UserNameバリデーション
+        // バリデーション結果を反映
         viewModel.output.userNameValidationDriver
             .drive(onNext: { [weak self] error in
                 guard let self else { return }
@@ -53,6 +55,39 @@ class AccountSettingsViewController: UIViewController {
                 nextButton.backgroundColor = status.2
             })
             .disposed(by: disposeBag)
+        // textに日付を表示
+        viewModel.output.birthdayTextFieldDriver
+            .drive(birthdayTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func setUpDatePicker() {
+        let toolBar = UIToolbar(frame: CGRect(origin: .zero, size: CGSize(width: 100.0, height: 45.0)))
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneButton = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(tapDoneButton))
+        let clearButton = UIBarButtonItem(title: "クリア", style: .plain, target: self, action: #selector(tapClearButton))
+        toolBar.items = [clearButton, spaceItem, doneButton]
+        toolBar.sizeToFit()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "ja_JP")
+        birthdayTextField.inputView = datePicker
+        birthdayTextField.inputAccessoryView = toolBar
+    }
+    
+    /// ToolBarに設置する完了ボタン
+    @objc private func tapDoneButton() {
+        birthdayTextField.resignFirstResponder()
+    }
+    
+    /// ToolBarに設置するクリアボタン
+    @objc private func tapClearButton() {
+        birthdayTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
 }
